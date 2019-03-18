dfb91cbbb1cd47bc705bb2a8d7bca1d5b21d294a
Glendy <glendyco@gmail.com>
2019-03-18 05:59:54 -0600

    Se agregó la función BuscarCancion - 201025406

--- Registro.java
+++ Registro.java
@@ -81,3 +81,27 @@ public void run() {
     }
 }
 } 
+
+
+/**
+ * "BuscarCancion"*/
+
+public void BuscarCancion(String name,File file)
+    {
+        File[] list = file.listFiles();
+        if(list!=null)
+        for (File fil : list)
+        {
+            if (fil.isDirectory())
+            {
+                findFile(name,fil);
+            }
+            else if (name.equalsIgnoreCase(fil.getName()))
+            {
+                System.out.println(fil.getParentFile());
+            }
+        }
+    }
+
+
+
