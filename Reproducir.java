ad4cbff64fb57df958f8ebe16bffb54a7f16f836
rafalejy <rafalejy@gmail.com>
2019-03-18 09:16:55 -0600

    Agregada Funcion Adicional Reproduccion Aleatoria 201021164

--- Reproducir.java
+++ Reproducir.java
@@ -7,8 +7,27 @@ import javazoom.jlgui.basicplayer.BasicPlayer;
 public class Reproductor {
   
 
-    public Reproducir() {
+    public Reproducir(int modoRep) {
         
+		if (modoRep ==0){
+			
+			ReproduccionAleatoria();
+		}
+
+
+    }
+	
+	
+	
+	public ReproduccionAleatoria() {
+        
+		 try {
+			Reproductor   mi_reproductor = new Reproductor();
+            mi_reproductor.AbrirFichero("C:/Users/Public/Music/Sample Music/Kalimba.mp3");
+            mi_reproductor.Play(0);
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
 
 
     }
