9e06bd6679f14fef55810b0950e87d7ec2f19056
rafalejy <rafalejy@gmail.com>
2019-03-18 10:58:50 -0600

    Agregadas Funciunalidades Subir y Descargar canción

--- Funcionalidades.java
+++ Funcionalidades.java
@@ -89,9 +89,62 @@ public class Funcionalidades{
 	
 
  	public SubirCancion() {
+		  File archivo = null;
+		  FileReader fr = null;
+		  BufferedReader br = null;
 
+		  try {
+			 // Apertura del fichero y creacion de BufferedReader para poder
+			 // hacer una lectura comoda (disponer del metodo readLine()).
+			 archivo = new File ("C:\\archivo.txt");
+			 fr = new FileReader (archivo);
+			 br = new BufferedReader(fr);
+
+			 // Lectura del fichero
+			 String linea;
+			 while((linea=br.readLine())!=null)
+				System.out.println(linea);
+		  }
+		  catch(Exception e){
+			 e.printStackTrace();
+		  }finally{
+			 // En el finally cerramos el fichero, para asegurarnos
+			 // que se cierra tanto si todo va bien como si salta 
+			 // una excepcion.
+			 try{                    
+				if( null != fr ){   
+				   fr.close();     
+				}                  
+			 }catch (Exception e2){ 
+				e2.printStackTrace();
+			 }
+		  }
 	}
 
+	public DescargarCancion() {
+		FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("c:/prueba.txt");
+            pw = new PrintWriter(fichero);
+
+            for (int i = 0; i < 10; i++)
+                pw.println("Linea " + i);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+	}
 
 	public VisualizarPlayList() {
 
