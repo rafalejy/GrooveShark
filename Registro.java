a9b1d1a67564cb2c4bbaaddeb24aa6d3aab3d744
rafalejy <rafalejy@gmail.com>
2019-03-17 22:58:22 -0600

    Creado Archivo Funcion Nuevo Usuario 201021164

---
+++ Registro.java
@@ -0,0 +1,57 @@
+
+import com.company.sample.service.RegistrationService;
+import com.haulmont.cuba.core.global.validation.MethodParametersValidationException;
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.components.PasswordField;
+import com.haulmont.cuba.gui.components.TextField;
+
+import javax.inject.Inject;
+
+/**
+ * "Registrar nuevo usuario"*/
+ 
+public class RegisrarNuevoUsuario extends AbstractWindow {
+
+    @Inject
+    private TextField loginField;
+
+    @Inject
+    private PasswordField passwordField;
+
+    @Inject
+    private RegistrationService registrationService;
+
+    /**
+     * "Cancel" button click handler.
+     */
+    public void onCancelBtnClick() {
+        close(CLOSE_ACTION_ID);
+    }
+
+    /**
+     * "OK" button click handler.
+     */
+    public void onOkBtnClick() {
+        try {
+            registrationService.registerUser(getLogin(), getPassword());
+            showNotification("Created user " + getLogin(), NotificationType.TRAY);
+            close(COMMIT_ACTION_ID);
+        } catch (MethodParametersValidationException e) {
+            showNotification(messages.getMessage("com.company.sample.validation", "UserExistsValidator.message"), NotificationType.TRAY);
+        }
+    }
+
+    /**
+     * @return entered login
+     */
+    public String getLogin() {
+        return loginField.getValue();
+    }
+
+    /**
+     * @return entered password
+     */
+    public String getPassword() {
+        return passwordField.getValue();
+    }
+}
