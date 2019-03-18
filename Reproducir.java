e3155e09746d98a4298dc168a3d9d754cfe768c2
Glendy <glendyco@gmail.com>
2019-03-18 15:24:48 -0600

    Se agregó el metodo ReproducirNoAleatorio -201025406

--- Reproducir.java
+++ Reproducir.java
@@ -9,6 +9,35 @@ public class Reproductor {
 
     public Reproducir() {
         
+	if (modRep = 1){
+	ReproducciónNoAleatoria();
+	}
+
+    }
+
+
+    public ReproduccionNoAleatoria() {
+ 	try {
+            
+            // Se obtiene un Clip de sonido
+            Clip sonido = AudioSystem.getClip();
+            
+            // Se carga con un fichero wav
+            sonido.open(AudioSystem.getAudioInputStream(new File("wav2.wav")));
+            
+            // Comienza la reproducción
+            sonido.start();
+            
+            // Espera mientras se esté reproduciendo.
+            while (sonido.isRunning())
+                Thread.sleep(1000);
+            
+            // Se cierra el clip.
+            sonido.close();
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+
 
 
     }
